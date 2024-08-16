cf7ad2f124ec78ab3fb87044f69945a1ce154ac6
LKEWXD <89427766+LKEWXD@users.noreply.github.com>
2024-08-15 23:43:03 -0500

    test branch commit
    added pytest tests in test_television.py
    added docustrings and type hinting in television.py

--- television.py
+++ television.py
@@ -4,50 +4,79 @@ class Television:
     MIN_CHANNEL = 0
     MAX_CHANNEL = 3
 
-    def __init__(self):
+    def __init__(self) -> None:
+        '''
+        Method to set default values of Television object
+        :param self:
+        '''
         self.status = False
         self.muted = False
         self.volume = Television.MIN_VOLUME
         self.channel = Television.MIN_CHANNEL
 
-    def power(self):
+    def power(self) -> None:
+        '''
+        Toggles boolean in self.status to represent power state
+        :param self:
+        '''
         self.status = not self.status
 
-    def mute(self):
+    def mute(self) -> None:
+        '''
+        Toggles boolean in self.muted to represent muted state
+        :param self:
+        '''
         if self.status:
             self.muted = not self.muted
 
-
-    def channel_up(self):
-
+    def channel_up(self) -> None:
+        '''
+        Increases self.channel if it is within acceptable range
+        :param self:
+        '''
+        if self.status:
             if self.channel == Television.MAX_CHANNEL:
                 self.channel = Television.MIN_CHANNEL
             else:
                 self.channel += 1
 
-
-    def channel_down(self):
+    def channel_down(self) -> None:
+        '''
+        Decreases self.channel if it is within acceptable range
+        :param self:
+        '''
         if self.status:
             if self.channel == Television.MIN_CHANNEL:
                 self.channel = Television.MAX_CHANNEL
             else:
                 self.channel -= 1
 
-
-    def volume_up(self):
+    def volume_up(self) -> None:
+        '''
+        Increases self.volume if it is within acceptable range and sets self.muted false
+        :param self:
+        '''
         if self.status:
             self.muted = False
             if self.volume < Television.MAX_VOLUME:
                 self.volume += 1
 
-
-    def volume_down(self):
+    def volume_down(self) -> None:
+        '''
+        Decreases self.volume if it is within acceptable range and sets self.muted false
+        :param self:
+        '''
         if self.status:
             self.muted = False
             if self.volume > Television.MIN_VOLUME:
                 self.volume -= 1
 
-    def __str__(self):
+    def __str__(self) -> str:
+        '''
+        returns variables self.status, self.channel, and self.volume in a string
+        :param self:
+        :return: string containing variable states
+        '''
         if self.muted:
             vol = 0
         else:
